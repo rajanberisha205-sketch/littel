039c840b6f5aec2e75aee567d0069d2572f27799
Rajan <rajanberisha205@gmail.com>
2026-09-14 16:51:46 +0200

    Refactor service worker for fetch requests
    Removed caching mechanism and updated fetch handling.

--- sw.js
+++ sw.js
@@ -1,22 +1,12 @@
-const CACHE_NAME = 'fasqoo-lite-v1';
-const assetsToCache = [
-  '/',
-  '/index.html',
-  '/fasqoologo.png'
-];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assetsToCache);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  // Leitet Anfragen normal durch
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
